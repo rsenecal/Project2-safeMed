883575fac216a237af48e8fe8f4b62a36b3d53fe
briabr <filalibria@yahoo.fr>
2022-11-09 12:42:31 -0500

    created med model

---
+++ models/med.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Med extends Model {}
+
+Med.init(
+
+{
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+  
+        maker: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+         
+        },
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Med'
+  }
+);
+
+module.exports = Med;
