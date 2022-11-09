bda516f2de07965e7eb5dbe3906f9bd517e482ce
rsenecal <roodysenecal@gmail.com>
2022-11-09 15:19:14 -0500

    fixed to eslint

--- server.js
+++ server.js
@@ -7,11 +7,33 @@ const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+  app.listen(PORT, () => console.log('Now listening'));
 });
 
 // const path = require('path');
