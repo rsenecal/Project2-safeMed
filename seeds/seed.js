e1ac6583a1c5b84012e73c8bfdba754d753bbb0e
SindidR <sindidreedh@gmail.com>
2022-11-09 13:14:11 -0500

    addition-of-med-Data-to-seed-js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,11 @@
 const sequelize = require('../config/connection');
-const { User, Patient, Prescription, Pres_History } = require('../models');
+const { User, Patient, Prescription, Pres_History, Med } = require('../models');
 
 const userData = require('./userData.json');
 const patientData = require('./patientData.json');
-const prescriptionData = require('./patientData.json');
-const pres_history = require('./pres_history.json')
+const prescriptionData = require('./prescriptionData.json');
+const pres_historyData = require('./pres_historyData.json')
+const medData = require('./medData.json')
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
