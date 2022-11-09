d0b162043703c7cbde44f564de496e2304006057
Evince Edouard <studioimac@evinces-imac.mynetworksettings.com>
2022-11-09 11:25:54 -0500

    authmiddleware file added

---
+++ controllers/home-routes.js
@@ -0,0 +1,9 @@
+//middleware / add to util folder
+// const checkAuth = (req, res, next) => {
+//     if (!req.session.loggedIn) {
+//       res.redirect('/login');
+//     } 
+//     next()
+// }
+
+//checkauth function
