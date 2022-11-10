b7017a95c96163761bdff0556e3951f7416af9a6
rsenecal <roodysenecal@gmail.com>
2022-11-10 11:22:10 -0500

    Seeding database is working

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const sequelize = require('./config/connection');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,7 +16,11 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+sequelize.sync({ force: false })
+  .then (() =>{
+    app.listen(PORT, () =>
+      console.log(`Example app listening at http://localhost:${PORT}`));
 
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+  });
+
+
