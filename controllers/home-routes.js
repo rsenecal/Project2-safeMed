c3f22ce1fd193e92d80594dd5b495b796ec96a40
JAG <jagrasso42@gmail.com>
2022-11-10 11:35:18 -0500

    added express-handlebars and middleware to render views, changed homeroutes endpoints

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 
 //GET homepage
-router.get("/", (req, res) => {
-    res.render("home", {layout: "homepage"})
-    //render 
-})
-
-
-
+router.get('/', (req, res) => {
+  res.render('homepage', { layout: 'main' });
+  //render
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const path = require('path');
+const routes = require('./controllers');
+
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // TODO: Invoke app.use() and serve static files from the '/public' folder
 app.use(express.static('public'));
+
+app.use(routes);
 
 // Leave these for now, I may need them later -Jeff
 // app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 // app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/index.html'))
+// );
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
