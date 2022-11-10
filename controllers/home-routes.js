86bc6b3b8c22e173d4d94cf048829bd0c9d02cfd
briabr <filalibria@yahoo.fr>
2022-11-09 21:39:57 -0500

    installed express and sequelize

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 
 //GET homepage
-router.get("/", (req, res) => {
-    res.render("home", {layout: "homepage"})
-    //render 
-})
+router.get('/', (req, res) => {
+    res.render('home', {layout: 'homepage'});
+});
 
 
 
