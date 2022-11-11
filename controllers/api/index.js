d8997b45c351d5fb83fd42e6ca738ee489e08662
rsenecal <roodysenecal@gmail.com>
2022-11-10 23:20:13 -0500

    Small change to the api index.js to make sure we comment user auth until login is active

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,5 +4,9 @@ const userRoutes = require('./user-routes');
 const patientRoutes = require('./patientRoutes');
 
 router.use('/users', userRoutes);
+//  *** Once login is working Uncomment the line below ***
+// router.use('/patients', checkAuth, patientRoutes);
+
+// ***** Once user login is working comment the line below. ****
 router.use('/patients', patientRoutes);
 module.exports = router;
