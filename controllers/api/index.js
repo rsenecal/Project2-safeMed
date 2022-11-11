e4bcf4826c2dad5a4d9d8c4a80e9fea65bd8c5e2
briabr <filalibria@yahoo.fr>
2022-11-11 11:59:55 -0500

    changed the api files t have same convention

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const { checkAuth } = require('../../middlewares/authMiddleware');
 const userRoutes = require('./user-routes');
-const patientRoutes = require('./patientRoutes');
-const medRoutes = require('./medRoutes');
+const patientRoutes = require('./patient-routes');
+const medRoutes = require('./med-routes');
 
 router.use('/users', userRoutes);
 //  *** Once login is working Uncomment the line below ***
