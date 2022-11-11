6af6d4527ccda8014c69baf2579a9d3576bb7be8
rsenecal <roodysenecal@gmail.com>
2022-11-11 00:49:42 -0500

    API Routes for Meds

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { checkAuth } = require('../../middlewares/authMiddleware');
 const userRoutes = require('./user-routes');
 const patientRoutes = require('./patientRoutes');
+const medRoutes = require('./medRoutes');
 
 router.use('/users', userRoutes);
 //  *** Once login is working Uncomment the line below ***
@@ -9,4 +10,6 @@ router.use('/users', userRoutes);
 
 // ***** Once user login is working comment the line below. ****
 router.use('/patients', patientRoutes);
+router.use('/meds', medRoutes);
+
 module.exports = router;
