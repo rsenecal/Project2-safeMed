9b2c4ee629eaa430b4fa710a9f73e276e3fcc368
rsenecal <roodysenecal@gmail.com>
2022-11-10 23:18:30 -0500

    Working API patientRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const { checkAuth } = require('../../middlewares/authMiddleware');
 const userRoutes = require('./user-routes');
+const patientRoutes = require('./patientRoutes');
 
 router.use('/users', userRoutes);
-
+router.use('/patients', patientRoutes);
 module.exports = router;
