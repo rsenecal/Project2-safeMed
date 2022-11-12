0ff311cb6694115db962b5dc6b176ce7c026f192
JAG <jagrasso42@gmail.com>
2022-11-12 15:27:58 -0500

    added endpoint for creating new potential customers

--- controllers/api/potential-customer-routes.js
+++ controllers/api/potential-customer-routes.js
@@ -37,4 +37,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
+// POST api/customers - add new potential customer
+router.post('/', async (req, res) => {
+  try {
+    const customerAdded = await PotentialCustomer.create({
+      ...req.body,
+    });
+    res.status(200).json(customerAdded);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
