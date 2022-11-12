141a8e227977bacdb2637568ab0a153b7993adf4
JAG <jagrasso42@gmail.com>
2022-11-12 15:51:49 -0500

    added seed script and seeds for potential customers

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,44 +1,51 @@
 const sequelize = require('../config/connection');
-const { User, Patient, Prescription, Med, PresHistory } = require('../models');
+const {
+  User,
+  Patient,
+  Prescription,
+  Med,
+  PresHistory,
+  PotentialCustomer,
+} = require('../models');
 
 const userData = require('./userData.json');
 const patientData = require('./patientData.json');
 const prescriptionData = require('./prescriptionData.json');
 // const presHistoryData = require('./presHistoryData.json');
 const medData = require('./medData.json');
+const potentialCustomerData = require('./potentialCustomerData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData,
-    {
-      individualHooks: true,
-      returning: true,
-    });
-
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   const patients = await Patient.bulkCreate(patientData, {
     // individualHooks: true,
     returning: true,
   });
 
-
   const meds = await Med.bulkCreate(medData, {
     // individualHooks: true,
     returning: true,
   });
 
-
+  await PotentialCustomer.bulkCreate(potentialCustomerData, {
+    // individualHooks: true,
+    returning: true,
+  });
 
   for (let prescription of prescriptionData) {
     await Prescription.create({
       ...prescription,
       user_id: users[Math.floor(Math.random() * users.length)].id,
       med_id: meds[Math.floor(Math.random() * users.length)].id,
-      patient_id: patients[Math.floor(Math.random() * users.length)].id
+      patient_id: patients[Math.floor(Math.random() * users.length)].id,
     });
   }
-
 
   process.exit(0);
 };
