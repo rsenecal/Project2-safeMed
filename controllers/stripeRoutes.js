5affc4db70ae3035a9dea7463621e4b98e710cc7
rsenecal <roodysenecal@gmail.com>
2022-11-12 17:25:38 -0500

    JAWSDB setup

--- controllers/stripeRoutes.js
+++ controllers/stripeRoutes.js
@@ -8,6 +8,11 @@ router.get('/', (req, res) => {
   res.render('stripepage', { stripePublishableKey: keys.stripePublishableKey });
 });
 
+
+router.get('/charge', (req, res) => {
+  res.render('stripecompleted',{layout: 'main'}, { stripePublishableKey: keys.stripePublishableKey });
+});
+
 // Charge Route
 router.post('/charge', (req, res) => {
   const amount = 1500;
@@ -22,7 +27,8 @@ router.post('/charge', (req, res) => {
       currency: 'usd',
       customer: customer.id
     }))
-    .then(charge => res.render('stripecompleted'));
+    .then(() => window.location.replace('/stripe/charge'));
+  // .then(() => res.render('stripecompleted', {layout: 'main'}));
 });
 
 module.exports = router;
