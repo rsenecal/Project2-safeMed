713b372ba32dd7014c3a2578ef31fc7c0f44cc16
briabr <filalibria@yahoo.fr>
2022-11-11 22:48:58 -0500

    create patient route, display patient first_name

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,9 +5,9 @@ router.get('/', (req, res) => {
   res.render('homepage', { layout: 'main' });
   //render
 });
-router.get('/patients', (req, res) => {
-  
 
-})
+router.get('/patients', (req, res) => {
+  res.render('patients');
+});
 
 module.exports = router;
