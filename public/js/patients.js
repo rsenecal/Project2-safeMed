9e4731d0e4780fed81ce42f2638b366626c283eb
briabr <filalibria@yahoo.fr>
2022-11-14 00:06:39 -0500

    edit patient functionality in javascript

--- public/js/patients.js
+++ public/js/patients.js
@@ -1,7 +1,19 @@
+const { json } = require("body-parser");
+
 function edit_patient(event){
     event.preventDefault();
-
+    let response = await fetch('/api/patient/1', {
+        method: 'PUT',
+        body: JSON.stringify({}),
+        headers: {'Content-Type':'application/json'}
+    })
+if (response.ok){
+document.location('/dashboard')
+}else {
+alert('failed to delete patient')
 }
+}
+
 
 aysnc function delete_patient(event){
     event.preventDefault();
